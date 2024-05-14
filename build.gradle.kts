959f4a87373ffe7847fd18575db3eeb281c2787f
Mason <masondunkle@gmail.com>
2024-05-13 21:14:32 -0400

    add publishing rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,14 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.masondkl.plinth"
+            artifactId = "Plinth"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
+}
